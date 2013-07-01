109f32232cf3bfc23cc5891afe346dfedd3c8306
bmoths <bmoths@toan.uchicago.edu>
2013-07-01 16:00:42 -0500

    Hard wall geometry extending abstractgeometry

--- src/Engine/HardWallSystemGeometry.java
+++ src/Engine/HardWallSystemGeometry.java
@@ -10,12 +10,7 @@ import java.util.Random;
  *
  * @author bmoths
  */
-public class HardWallSystemGeometry {
-
-    int dimension;
-    double[] fullRMax;
-    public static Random randomNumberGenerator = new Random();
-    private SimulationParameters parameters;
+public class HardWallSystemGeometry extends AbstractGeometry {
 
     public HardWallSystemGeometry() {
         dimension = 2;
@@ -23,6 +18,7 @@ public class HardWallSystemGeometry {
         parameters = new SimulationParameters();
     }
 
+    @Override
     public boolean isPositionValid(double[] position) {
         for (int i = 0; i < dimension; i++) {
             if (position[i] < 0 || position[i] > fullRMax[i]) {
@@ -32,6 +28,7 @@ public class HardWallSystemGeometry {
         return true;
     }
 
+    @Override
     public boolean isSumInBounds(double[] position, double[] translation) {
         double sumCoordinate;
         for (int i = 0; i < dimension; i++) {
@@ -43,36 +40,7 @@ public class HardWallSystemGeometry {
         return true;
     }
 
-    public void setDimension(int dimension) {
-        if (dimension > 2 || dimension <= 3) {
-            this.dimension = dimension;
-        }
-    }
-
-    public int getDimension() {
-        return dimension;
-    }
-
-    public void setDimensionSize(int dimensionToBeSized, double size) {
-        if (dimensionToBeSized >= 0 && dimensionToBeSized < 3 && size > 0) {
-            fullRMax[dimensionToBeSized] = size;
-        }
-    }
-
-    public double[] getRMax() {
-        double[] rMax = new double[dimension];
-        System.arraycopy(fullRMax, 0, rMax, 0, dimension);
-        return rMax;
-    }
-
-    public SimulationParameters getParameters() {
-        return parameters;
-    }
-
-    public void setParameters(SimulationParameters parameters) {
-        this.parameters = parameters;
-    }
-
+    @Override
     public double sqDist(double[] position1, double[] position2) {
         double sqDist = 0;
         for (int i = 0; i < dimension; i++) {
@@ -81,6 +49,7 @@ public class HardWallSystemGeometry {
         return sqDist;
     }
 
+    @Override
     public double areaOverlap(double[] position1, double[] position2) {
         double overlap = 1;
 
@@ -91,36 +60,14 @@ public class HardWallSystemGeometry {
         return overlap;
     }
 
-    public double[] randomPosition() {
-        double[] position = new double[dimension];
-        for (int i = 0; i < dimension; i++) {
-            position[i] = randomNumberGenerator.nextDouble() * fullRMax[i];
-        }
-        return position;
-    }
-
-    public double[][] randomPositions(int numPositions) {
-        double[][] randomPositions = new double[numPositions][];
-        for (int i = 0; i < numPositions; i++) {
-            randomPositions[i] = randomPosition();
-        }
-        return randomPositions;
-    }
-
-    public double[] randomGaussian() {
-        double[] randomVector = new double[dimension];
-        for (int i = 0; i < dimension; i++) {
-            randomVector[i] = randomNumberGenerator.nextGaussian() * parameters.getStepLength();
-        }
-        return randomVector;
-    }
-
+    @Override
     public void doStep(double[] toStep, double[] stepVector) {
         for (int i = 0; i < dimension; i++) {
             toStep[i] += stepVector[i];
         }
     }
 
+    @Override
     public void undoStep(double[] toStep, double[] stepVector) {
         for (int i = 0; i < dimension; i++) {
             toStep[i] -= stepVector[i];
