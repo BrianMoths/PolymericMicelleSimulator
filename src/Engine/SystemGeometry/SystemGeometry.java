add51063bfaa0e8d4b619bdd6ca8c86d7be8f878
bmoths <bmoths@toan.uchicago.edu>
2013-07-02 11:19:59 -0500

    Interface for a system geometry

---
+++ src/Engine/SystemGeometry/SystemGeometry.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Engine.SystemGeometry;
+
+import Engine.SimulationParameters;
+
+/**
+ *
+ * @author bmoths
+ */
+public interface SystemGeometry {
+
+    public boolean isPositionValid(double[] position);
+
+    public boolean isSumInBounds(double[] position, double[] translation);
+
+    public void setDimension(int dimension);
+
+    public int getDimension();
+
+    public void setDimensionSize(int dimensionToBeSized, double size);
+
+    public double[] getRMax();
+
+    public double getVolume();
+
+    public SimulationParameters getParameters();
+
+    public void setParameters(SimulationParameters parameters);
+
+    public double sqDist(double[] position1, double[] position2);
+
+    public double areaOverlap(double[] position1, double[] position2);
+
+    public double[] randomPosition();
+
+    public double[][] randomPositions(int numPositions);
+
+    public double[] randomGaussian();
+
+    public void doStep(double[] toStep, double[] stepVector);
+
+    public void undoStep(double[] toStep, double[] stepVector);
+}
