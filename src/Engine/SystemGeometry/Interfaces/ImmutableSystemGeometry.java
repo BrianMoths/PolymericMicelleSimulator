940253fdbb8c41aeb33f54b8dbfb63e12ad9d650
bmoths <bmoths@toan.uchicago.edu>
2013-11-26 13:25:10 -0600

    added an ImmutableGeometry interface, had SystemGeometry extend it.

---
+++ src/Engine/SystemGeometry/Interfaces/ImmutableSystemGeometry.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Engine.SystemGeometry.Interfaces;
+
+import Engine.SystemGeometry.GeometricalParameters;
+import Engine.TwoBeadOverlap;
+import SystemAnalysis.AreaPerimeter.BeadRectangle;
+import SystemAnalysis.AreaPerimeter.RectangleSplitting.RectanglesAndGluedPerimeter;
+import java.util.List;
+
+/**
+ *
+ * @author bmoths
+ */
+public interface ImmutableSystemGeometry {
+
+    public GeometryBuilder toBuilder();
+
+    public boolean isPositionValid(double[] position);
+
+    public int getDimension();
+
+    public double[] getRMax();
+
+    public double getVolume();
+
+    public GeometricalParameters getParameters();
+
+    public double sqDist(double[] position1, double[] position2);
+
+    public TwoBeadOverlap twoBeadOverlap(double[] position1, double[] position2);
+
+    public double[] randomPosition();
+
+    public double[][] randomPositions(int numPositions);
+
+    public double[] randomColumnPosition();
+
+    public double[][] randomColumnPositions(int numPositions);
+
+    /**
+     *
+     * @return a random vector whose components are normally distributed with
+     * standard deviation of step length
+     */
+    public double[] randomGaussian();
+
+    /**
+     *
+     * @param scaleFactor factor by which to scale the Gaussian
+     * @return a random vector whose components are normally distributed with
+     * standard deviation of step length times the scale factor.
+     */
+    public double[] randomGaussian(double scaleFactor);
+
+    public boolean incrementFirstVector(double[] toStep, double[] stepVector);
+
+    public void decrementFirstVector(double[] toStep, double[] stepVector);
+
+    public void checkedCopyPosition(double[] src, double[] dest);
+
+    public void checkedCopyPositions(double[][] src, double[][] dest);
+
+    public RectanglesAndGluedPerimeter getRectanglesAndPerimeterFromPositions(double[][] beadPostions);
+
+    public List<BeadRectangle> getRectanglesFromPositions(double[][] beadPositions);
+
+}
--- src/Engine/SystemGeometry/Interfaces/SystemGeometry.java
+++ src/Engine/SystemGeometry/Interfaces/SystemGeometry.java
@@ -4,71 +4,14 @@
  */
 package Engine.SystemGeometry.Interfaces;
 
-import Engine.SystemGeometry.GeometricalParameters;
-import Engine.SystemGeometry.Interfaces.GeometryBuilder;
-import Engine.TwoBeadOverlap;
-import SystemAnalysis.AreaPerimeter.BeadRectangle;
-import SystemAnalysis.AreaPerimeter.RectangleSplitting.RectanglesAndGluedPerimeter;
 import java.io.Serializable;
-import java.util.List;
 
 /**
  *
  * @author bmoths
  */
-public interface SystemGeometry extends Serializable {
-
-    public GeometryBuilder toBuilder();
-
-    public boolean isPositionValid(double[] position);
-
-    public int getDimension();
-
-    public double[] getRMax();
+public interface SystemGeometry extends ImmutableSystemGeometry, Serializable {
 
     public void setRMax(int index, double rMax);
-
-    public double getVolume();
-
-    public GeometricalParameters getParameters();
-
-    public double sqDist(double[] position1, double[] position2);
-
-    public TwoBeadOverlap twoBeadOverlap(double[] position1, double[] position2);
-
-    public double[] randomPosition();
-
-    public double[][] randomPositions(int numPositions);
-
-    public double[] randomColumnPosition();
-
-    public double[][] randomColumnPositions(int numPositions);
-
-    /**
-     *
-     * @return a random vector whose components are normally distributed with
-     * standard deviation of step length
-     */
-    public double[] randomGaussian();
-
-    /**
-     *
-     * @param scaleFactor factor by which to scale the Gaussian
-     * @return a random vector whose components are normally distributed with
-     * standard deviation of step length times the scale factor.
-     */
-    public double[] randomGaussian(double scaleFactor);
-
-    public boolean incrementFirstVector(double[] toStep, double[] stepVector);
-
-    public void decrementFirstVector(double[] toStep, double[] stepVector);
-
-    public void checkedCopyPosition(double[] src, double[] dest);
-
-    public void checkedCopyPositions(double[][] src, double[][] dest);
-
-    public RectanglesAndGluedPerimeter getRectanglesAndPerimeterFromPositions(double[][] beadPostions);
-
-    public List<BeadRectangle> getRectanglesFromPositions(double[][] beadPositions);
 
 }
